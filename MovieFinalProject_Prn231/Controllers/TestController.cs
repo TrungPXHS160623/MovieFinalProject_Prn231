ff77c81e8c47577f1e23b6426b90c105b3ac4b9f
TrungPXHS160623 <trungpxhs160623@fpt.edu.vn>
2024-10-02 00:57:24 +0700

    Second Commit

---
+++ MovieFinalProject_Prn231/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MovieFinalProject_Prn231.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public TestController()
+        {
+            
+           
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("HelloWorld!!!");
+        }
+    }
+}
